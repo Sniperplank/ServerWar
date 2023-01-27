92ec22f1ec968255831cdb51861dfb9016e5b9ef
Eilaf Aljundi <elafjundi@gmail.com>
2023-01-27 12:03:24 -0500

    cooldown bugs fixed

--- index.js
+++ index.js
@@ -47,30 +47,30 @@ client.on('interactionCreate', async interaction => {
     let profileData = await Profile.findOne({ userID: interaction.user.id, serverID: interaction.guild.id })
 
     // If cooldowns map doesnt have a command.name key then create one.
-    if (command.cooldown) {
-        if (!cooldowns.has(command.name)) {
-            cooldowns.set(command.name, new Discord.Collection());
-        }
-
-        const current_time = Date.now();
-        const time_stamps = cooldowns.get(command.name);
-        const cooldown_amount = (command.cooldown) * 1000;
-
-        // If time_stamps has a key with the author's id then check the expiration time to send a message to a user.
-        if (time_stamps.has(interaction.user.id)) {
-            const expiration_time = time_stamps.get(interaction.user.id) + cooldown_amount;
-
-            if (current_time < expiration_time) {
-                const time_left = (expiration_time - current_time) / 1000;
-
-                return interaction.reply(`Please wait ${time_left.toFixed(1)} more seconds before using ${command.name}`);
+    if (profileData) {
+        if (command.cooldown) {
+            if (!cooldowns.has(command.name)) {
+                cooldowns.set(command.name, new Discord.Collection());
             }
-        }
 
-        // If the author's id is not in time_stamps then add them with the current time.
-        time_stamps.set(interaction.user.id, current_time);
-        // Delete the user's id once the cooldown is over.
-        setTimeout(() => time_stamps.delete(interaction.user.id), cooldown_amount);
+            const current_time = Date.now();
+            const time_stamps = cooldowns.get(command.name);
+            const cooldown_amount = (command.cooldown) * 1000;
+            // If time_stamps has a key with the author's id then check the expiration time to send a message to a user.
+            if (time_stamps.has(interaction.user.id + interaction.guild.id)) {
+                const expiration_time = time_stamps.get(interaction.user.id + interaction.guild.id) + cooldown_amount;
+
+                if (current_time < expiration_time) {
+                    const time_left = (expiration_time - current_time) / 1000;
+
+                    return interaction.reply(`You must wait ${time_left.toFixed()} more seconds before using /${command.name}`);
+                }
+            }
+            // If the author's id is not in time_stamps then add them with the current time.
+            time_stamps.set(interaction.user.id + interaction.guild.id, current_time);
+            // Delete the user's id once the cooldown is over.
+            setTimeout(() => time_stamps.delete(interaction.user.id + interaction.guild.id), cooldown_amount);
+        }
     }
 
     try {
