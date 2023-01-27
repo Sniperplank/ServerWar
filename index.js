7d34cfb1ad60f9f25401fdac95ca3a59f162acd3
Eilaf Aljundi <elafjundi@gmail.com>
2023-01-27 01:23:05 -0500

    command cooldowns

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Discord = require('discord.js');
 const Client = require('./client/Client');
 const { EmbedBuilder } = require('discord.js');
 const Profile = require('./models/profile')
+const cooldowns = new Map();
 
 
 const client = new Client();
@@ -44,6 +45,33 @@ client.on('messageCreate', async message => {
 client.on('interactionCreate', async interaction => {
     const command = client.commands.get(interaction.commandName.toLowerCase());
     let profileData = await Profile.findOne({ userID: interaction.user.id, serverID: interaction.guild.id })
+
+    // If cooldowns map doesnt have a command.name key then create one.
+    if (command.cooldown) {
+        if (!cooldowns.has(command.name)) {
+            cooldowns.set(command.name, new Discord.Collection());
+        }
+
+        const current_time = Date.now();
+        const time_stamps = cooldowns.get(command.name);
+        const cooldown_amount = (command.cooldown) * 1000;
+
+        // If time_stamps has a key with the author's id then check the expiration time to send a message to a user.
+        if (time_stamps.has(interaction.user.id)) {
+            const expiration_time = time_stamps.get(interaction.user.id) + cooldown_amount;
+
+            if (current_time < expiration_time) {
+                const time_left = (expiration_time - current_time) / 1000;
+
+                return interaction.reply(`Please wait ${time_left.toFixed(1)} more seconds before using ${command.name}`);
+            }
+        }
+
+        // If the author's id is not in time_stamps then add them with the current time.
+        time_stamps.set(interaction.user.id, current_time);
+        // Delete the user's id once the cooldown is over.
+        setTimeout(() => time_stamps.delete(interaction.user.id), cooldown_amount);
+    }
 
     try {
         command.execute(interaction, client, profileData);
